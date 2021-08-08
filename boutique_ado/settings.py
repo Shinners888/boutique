789a7b645a6bf2e47d706b0b9b4cf0433054566f
Shinners888 <sinead888888888@gmail.com>
2021-08-08 00:38:28 +0000

    changed secret key

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'btq2wds!z4ewzjeyc%7k!72f=#d%&s(91=2t^*-3^8)z480i_c'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['boutiqueadosinead.herokuapp.com', 'localhost']
 
