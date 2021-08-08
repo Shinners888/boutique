39717f73fc583627ca68d600ab4041bbef7032f6
Shinners888 <sinead888888888@gmail.com>
2021-08-08 20:37:39 +0000

    added static cache

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -180,6 +180,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'boutiqueadosinead'
     AWS_S3_REGION_NAME = 'eu-west-1'
